f8e1238db098ff42c1648d6a5aacd6dc06a3555c
Juan Tascon <juantascon@gmail.com>
2011-12-22 08:12:46 -0500

    fixed for distribution

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@
 from setuptools import setup
 import os
 
+
 setup (
     name="tvcmd",
     version="0.1",
     description="Command line tool to keep track of tv shows",
-    long_description="",
     author="Juan Tascón",
     author_email="juantascon@gmail.com",
     maintainer="Juan Tascón",
     maintainer_email="juantascon@gmail.com",
     url="https://sourceforge.net/projects/tvcmd/",
     keywords=["tvcmd", "episodes", "tv", "shows", "cli"],
+    
     packages=["tvcmd"],
-    scripts=["tvcmd.py"],
-    data_files=[(os.path.join("share", "tvcmd"), ["README", os.path.join("share", "tvcmd.svg")])],
+    scripts=["script/tvcmd"],
+    data_files=[("share/tvcmd", ["README", "share/tvcmd.svg"])],
     install_requires=[],
     license="GPL, Version 3.0" )
--- tvcmd/cli.py
+++ tvcmd/cli.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python
 
 import readline, cmd
 from tvcmd import torrent, episode, manager
