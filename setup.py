9959dcbcd16bcceaab862708efd441508066c835
Juan Tascon <juantascon@gmail.com>
2011-12-22 14:03:47 -0500

    updated setup.py and PKGBUILD to v0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup (
     name="tvcmd",
-    version="0.2",
+    version="0.3",
     description="Command line tool to keep track of tv shows",
     author="Juan Tascón",
     author_email="juantascon@gmail.com",
