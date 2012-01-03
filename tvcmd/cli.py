201a65d9d073153a35c8be56065543fe6243047f
Juan Tascon <juantascon@gmail.com>
2012-01-03 17:04:29 -0500

    command:* better argparsing and better help

--- tvcmd/cli.py
+++ tvcmd/cli.py
@@ -43,7 +43,10 @@ class Cmd(cmd.Cmd, manager.Manager):
         self.modified = False
         
     def do_reload(self, line):
-        """Reload episodes lists and reset their status\n\nSyntax:\n reload"""
+        parser = ArgumentParser(prog="reload", description="Reload episodes list and status", epilog="example: reload")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
+        
         if self.modified:
             answer = self.ask_yn("Database has been modified. Are you sure you want to reload?")
             if not answer: return
@@ -62,43 +65,65 @@ class Cmd(cmd.Cmd, manager.Manager):
             return False
     
     def do_save(self, line):
-        """Save episodes status DB\n\nSyntax:\n save"""
+        parser = ArgumentParser(prog="save", description="Save episodes status DB", epilog="example: save")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
+        
         self.save()
     
     #
     # Show Commands
     #
     def do_shows(self, line):
-        """Print tracked shows list\n\nSyntax:\n shows"""
+        parser = ArgumentParser(prog="shows", description="List shows information", epilog="example: shows")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
+        
         db = self.show_db
         print("\n%s\n"%(db.fmt()))
     
     def do_search(self, line):
-        """Search for shows in thetvdb.com database\n\nSyntax:\n search <TEXT>\nExample:\n search the office"""
-        msg("Searching [%s] ... "%(line))
-        try:
-            db = self.search_shows(line)
-        except ServerError as ex: msg("FAIL: (%s)\n"%(ex))
+        parser = ArgumentParser(prog="shows", description="Search for shows in thetvdb.com database", epilog="example: search the office")
+        parser.add_argument("filter", metavar="SHOW", help="show name or part, ex: the offi")
+        try: args = parser.parse_args([line])
+        except SystemExit: return
         
-        msg("OK: %d shows found\n"%(len(db)))
-        print("\n%s\n"%(db.fmt()))
-
+        msg("Searching [%s] ... "%(args.filter))
+        try:
+            db = self.search_shows(args.filter)
+            msg("OK: %d shows found\n"%(len(db)))
+            if (len(db)): print("\n%s\n"%(db.fmt()))
+        except ServerError as ex:
+            msg("FAIL: (%s)\n"%(ex))
+    
+    #
+    # Complete text
+    #
+    def _complete(self, text, options, db):
+        _options = [ opt for opt in options if opt.startswith(text) ]
+        _episodes = db.complete_text(text)
+        
+        return _options + _episodes
+    
+    def complete_tor(self, text, line, start_index, end_index):
+        return self._complete(text, ["-h", "--help"], self.episode_db.filter(lambda url: not url.future() and url["status"] in [cons.NEW]))
+    
+    def complete_ls(self, text, line, start_index, end_index):
+        return self._complete(text, ["-n", "--new", "-a", "--adquired", "-s", "--seen", "-f", "--future"], self.episode_db)
+    
+    def complete_new(self, text, line, start_index, end_index):
+        return self._complete(text, ["-h", "--help"], self.episode_db.filter(lambda url: url["status"] in [cons.ADQUIRED, cons.SEEN]))
+    
+    def complete_adquire(self, text, line, start_index, end_index):
+        return self._complete(text, ["-h", "--help"], self.episode_db.filter(lambda url: url["status"] in [cons.NEW]))
+    
+    def complete_see(self, text, line, start_index, end_index):
+        return self._complete(text, ["-h", "--help"], self.episode_db.filter(lambda url: url["status"] in [cons.NEW, cons.ADQUIRED]))
+    
     #
     # Episode Status Commands
     #
-    def complete_new(self, text, line, start_index, end_index):
-        db = self.episode_db.filter(lambda url: url["status"] in [cons.ADQUIRED, cons.SEEN])
-        return db.complete_text(text)
-    
-    def complete_adquire(self, text, line, start_index, end_index):
-        db = self.episode_db.filter(lambda url: url["status"] in [cons.NEW])
-        return db.complete_text(text)
-    
-    def complete_see(self, text, line, start_index, end_index):
-        db = self.episode_db.filter(lambda url: url["status"] in [cons.NEW, cons.ADQUIRED])
-        return db.complete_text(text)
-    
-    def mark(self, edb, status):
+    def _mark(self, edb, status):
         msg("Marking %d episode(s) as %s:\n"%(len(edb), cons.ENUM_EPISODE_STATUS[status]))
         if len(edb): self.modified = True
         
@@ -107,58 +132,56 @@ class Cmd(cmd.Cmd, manager.Manager):
             print(eurl.fmt_color())
     
     def do_new(self, line):
-        """Mark episodes as NEW\n\nSyntax:\n new <EPISODE> ... \nExample:\n new lost.s01* lost.s02*"""
+        parser = ArgumentParser(prog="new", description="Mark episodes as NEW", epilog="example: new lost.s01* lost.s02*")
+        parser.add_argument("filters", metavar="EPISODE", nargs="*", default=["*"], help="episode name or filter, ex: lost.s01e0*")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
+        
         db = episode.DB()
-        for pattern in line.split(" "):
-            if not pattern: continue
+        for pattern in args.filters:
             db.extend(self.episode_db.filter(lambda url: url.match(pattern) and url["status"] in [cons.ADQUIRED, cons.SEEN]))
         
-        self.mark(db, cons.NEW)
+        self._mark(db, cons.NEW)
     
     def do_adquire(self, line):
-        """Mark episodes as ADQUIRED\n\nSyntax:\n adquire <EPISODE> ... \nExample:\n adquire lost.s01* lost.s02*"""
+        parser = ArgumentParser(prog="adquire", description="Mark episodes as ADQUIRED", epilog="example: adquire lost.s01* lost.s02*")
+        parser.add_argument("filters", metavar="EPISODE", nargs="*", default=["*"], help="episode name or filter, ex: lost.s01e0*")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
+        
         db = episode.DB()
-        for pattern in line.split(" "):
-            if not pattern: continue
+        for pattern in args.filters:
             db.extend(self.episode_db.filter(lambda url: url.match(pattern) and url["status"] in [cons.NEW]))
         
-        self.mark(db, cons.ADQUIRED)
+        self._mark(db, cons.ADQUIRED)
     
     def do_see(self, line):
-        """Mark episodes as SEEN\n\nSyntax:\n see <EPISODE> ...\nExample:\n see lost.s01* lost.s02*"""
+        parser = ArgumentParser(prog="see", description="Mark episodes as SEEN", epilog="example: see lost.s01* lost.s02*")
+        parser.add_argument("filters", metavar="EPISODE", nargs="*", default=["*"], help="episode name or filter, ex: lost.s01e0*")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
+        
         db = episode.DB()
-        for pattern in line.split(" "):
-            if not pattern: continue
+        for pattern in args.filters:
             db.extend(self.episode_db.filter(lambda url: url.match(pattern) and url["status"] in [cons.NEW, cons.ADQUIRED]))
         
-        self.mark(db, cons.SEEN)
+        self._mark(db, cons.SEEN)
     
     #
     # Episode info Commands
     #
-    def complete_tor(self, text, line, start_index, end_index):
-        db = self.episode_db.filter(lambda url: not url.future() and url["status"] in [cons.NEW])
-        return db.complete_text(text)
-    
     def do_tor(self, line):
-        if not line: line = "*"
+        parser = ArgumentParser(prog="tor", description="Show torrent urls for NEW episodes", epilog="example: tor lost*")
+        parser.add_argument("filters", metavar="EPISODE", nargs="*", default=["*"], help="episode name or filter, ex: lost.s01e0*")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
         
         db = episode.DB()
-        for pattern in line.split(" "):
-            if not pattern: continue
-            db.extend(self.episode_db.filter(lambda url: not url.future() and url.match(pattern) and url["status"] in [cons.NEW]))
+        for pattern in args.filters:
+            db.extend(self.episode_db.filter(lambda url: not url.future() and url["status"] in [cons.NEW] and url.match(pattern)))
         
         for eurl in db:
             print(torrent.fmt_url(eurl["show"], eurl["season"], eurl["episode"]))
-    
-    def complete_ls(self, text, line, start_index, end_index):
-        db = self.episode_db
-        episodes = db.complete_text(text)
-        
-        options = ["-n", "--new", "-a", "--adquired", "-s", "--seen", "-f", "--future"]
-        options = [ opt for opt in options if opt.startswith(text) ]
-        
-        return  episodes + options
     
     def do_ls(self,line):
         parser = ArgumentParser(prog="ls", description="Show episodes information", epilog="example: ls -as lost*")
@@ -196,8 +219,16 @@ class Cmd(cmd.Cmd, manager.Manager):
             print(eurl.fmt_color())
     
     #
-    # Exit commands:
+    # Auxiliary commands:
     #
+    def do_help(self, line):
+        sep = "\n   "
+        msg("To get specific help type: COMMAND --help\n\n")
+        msg("Auxiliary commands:"+sep+sep.join(["exit", "quit", "help"]) + "\n")
+        msg("DB commands:"+sep+sep.join(["save", "reload"]) + "\n")
+        msg("Episodes commands:"+sep+sep.join(["new", "adquire", "see", "tor", "ls"]) + "\n")
+        msg("Shows commands:"+sep+sep.join(["shows", "search"]) + "\n")
+    
     def ask_yn(self, question):
         answer = ""
         while True:
@@ -214,18 +245,18 @@ class Cmd(cmd.Cmd, manager.Manager):
     def emptyline(self):
         pass
     
-    def do_exit(self, arg):
-        """Exit the application\n\nSyntax:\n exit"""
+    def do_exit(self, line):
+        parser = ArgumentParser(prog="exit/quit", description="Exit the application", epilog="example: exit")
+        try: args = parser.parse_args(line.split())
+        except SystemExit: return
+        
         return self.exit()
     
-    def do_quit(self, arg):
-        """Exit the application\n\nSyntax:\n quit"""
-        return self.exit()
+    do_quit = do_exit
     
     def default(self, line):
-        print()
-        
         if line == "EOF":
+            print()
             return self.exit()
         
         msg("Invalid command: %s"%(line.split(" ")[0]))
