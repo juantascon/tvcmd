4caa5761f42237695d27d5c180b62215e11d60ad
Juan Tascon <juantascon@horlux.org>
2013-10-18 12:02:05 -0500

    simplified thetvdb source using common functions

--- tvcmd/sources/thetvdb.py
+++ tvcmd/sources/thetvdb.py
@@ -1,8 +1,11 @@
 import urllib.parse
 import datetime
 
+import xml.parsers.expat
+
 from .. import errors, cons
 from . import base
+from ..lib import xmltodict
 
 #import zipfile, io
 import xml.etree.cElementTree as ElementTree
@@ -20,46 +23,35 @@ class TheTVDB(base.Base):
     def get_shows(self, pattern):
         url = "http://thetvdb.com/api/GetSeries.php?seriesname=%s" % (urllib.parse.quote(pattern))
         
-        try: root = self._get_xml(url)
-        except errors.ServerError as ex: raise errors.ServerError("Show not found (%s)"%(ex))
-        
-        l = []
-        for data in list(root):
-            if data.tag == "Series":
-                    info = { "name": None, "id": None }
-                    for s in list(data):
-                        if s.tag.lower() == "seriesid":
-                            info["id"] = s.text
-                        elif s.tag.lower() == "seriesname":
-                            info["name"] = s.text
-                    if info["id"]: l.append(info)
-        
-        return l
-        
+        try:
+            xml_content = self._get_url(url)
+            xml_content_dict = xmltodict.parse(xml_content)
+            xml_content_dict_shows = list(xml_content_dict["Data"].values())
+            return [{ "id": s["seriesid"], "name": s["SeriesName"]} for s in xml_content_dict_shows]
+        except xml.parsers.expat.ExpatError:
+            raise errors.SourceError("Invalid show: unexpected source response")
+        except:
+            raise
+            
     def get_episodes(self, show_id):
         url = "http://thetvdb.com/api/%s/series/%s/all/en.xml" % (cons.APIKEY, show_id)
         
-        try: root = self._get_xml(url)
-        except errors.ServerError as ex: raise errors.ServerError("Error getting episodes list (%s)"%(ex))
+        try:
+            xml_content = self._get_url(url)
+            xml_content_dict = xmltodict.parse(xml_content)
+            
+            l = []
+            #print(xml_content_dict["Data"]["Episode"])
+            for episode in xml_content_dict["Data"]["Episode"]:
+                l.append({
+                    "name": episode["EpisodeName"],
+                    "episode": int(episode["EpisodeNumber"]),
+                    "season": int(episode["SeasonNumber"]),
+                    "date": self._isostr_to_date(episode["FirstAired"])
+                })
+            return l
+        except xml_content.parsers.expat.ExpatError:
+            raise errors.SourceError("Invalid show id: unexpected source response")
+        except:
+            raise
         
-        l = []
-        for data in list(root):
-            if data.tag.lower() == "episode":
-                info = { "season": None, "episode": None, "name": "", "date": datetime.date.max }
-                
-                for e in list(data):
-                    if e.tag.lower() == "seasonnumber":
-                        info["season"] = int(e.text)
-                    elif e.tag.lower() == "episodenumber":
-                        info["episode"] = int(e.text)
-                    elif e.tag.lower() == "firstaired":
-                        try: info["date"] = datetime.date(int(e.text[0:4]), int(e.text[5:7]), int(e.text[8:10]))
-                        except: pass
-                    elif e.tag.lower() == "episodename":
-                        info["name"] = e.text
-                        
-                l.append(info)
-        
-        #log().debug(l)
-        #log().debug("\n")
-        return l
