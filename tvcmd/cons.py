7da7606423c3eb7efd6cfa05c3f58f78fe70ad0b
Juan Tascon <juantascon@horlux.org>
2014-04-10 09:37:24 -0500

    DEBUG DIR is now set in cons.py

--- tvcmd/cons.py
+++ tvcmd/cons.py
@@ -1,4 +1,4 @@
-import xdg.BaseDirectory
+import xdg.BaseDirectory, os
 
 # episodes status
 NEW = 0
@@ -20,7 +20,11 @@ ENUM_EPISODE_STATUS = {
 }
 
 # config
-CONFIGDIR = xdg.BaseDirectory.save_config_path("tvcmd")
+CONFIGDIRBASE = "tvcmd"
+if os.environ.get("DEBUG"):
+    CONFIGDIRBASE += "_debug"
+
+CONFIGDIR = xdg.BaseDirectory.save_config_path(CONFIGDIRBASE)
 MAINCONFIGFILE = CONFIGDIR + "/main.cfg"
 STATUSDBFILE = CONFIGDIR + "/status.db"
 CACHEFILE = CONFIGDIR + "/cache.db"
