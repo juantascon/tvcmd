c83bae3898080aa35864e94f0af052eb2550bcc9
Juan Tascon <juantascon@horlux.org>
2016-01-28 13:52:24 -0500

    using setuptools, added dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 from tvcmd import __author__, __author_email__, __version__, __license__
 
@@ -8,16 +8,20 @@ setup (
     name="tvcmd",
     version=__version__,
     description="Command line tool to keep track of tv shows",
+    url="http://tvcmd.horlux.org",
     author=__author__,
     author_email=__author_email__,
     maintainer=__author__,
     maintainer_email=__author_email__,
-    url="http://tvcmd.horlux.org",
+    license=__license__,
     keywords=["tvcmd", "episodes", "tv", "shows", "cli"],
+    
+    install_requires=[
+        'pyxdg',
+        'httplib2'
+    ],
     
     packages=["tvcmd", "tvcmd.lib", "tvcmd.sources"],
     scripts=["script/tvcmd"],
-    data_files=[("share/tvcmd", ["README.md", "LICENSE", "share/tvcmd.svg", "share/main.cfg.example"])],
-    
-    license=__license__
+    data_files=[("share/tvcmd", ["README.md", "LICENSE", "share/tvcmd.svg", "share/main.cfg.example"])]
 )
